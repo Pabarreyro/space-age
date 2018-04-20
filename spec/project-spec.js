89db1c723405638e16c2699d1600c6c76e5742c5
Pablo Barreyro <Pabarreyro@gmail.com>
2018-04-20 15:56:06 -0700

    Create test for calculating age in years (failing)

--- spec/project-spec.js
+++ spec/project-spec.js
@@ -1,7 +1,20 @@
-// import { convertToSeconds } from "./../src/space_age.js";
+import { convertYearsToSeconds } from "./../src/space_age.js";
 
 describe('Space Age Calculator', function() {
-  it('should convert one year to seconds', function() {
+  let testUser18;
+  let testUser90;
+
+  beforeEach(function() {
+    testUser18 = new SpaceAge(2000, 4, 20);
+    testUser90 = new SpaceAge(1928, 4, 20);
+  });
+
+  it('should convert 1 year to seconds', function() {
     expect(convertYearsToSeconds(1)).toEqual(3.1536e+7);
+  });
+
+  it('should calculate user age in seconds from birthday', function() {
+    expect(testUser18.calculateAge()).toEqual(18);
+    expect(testUser18.calculateAge()).toEqual(90);
   });
 });
