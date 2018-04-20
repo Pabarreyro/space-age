4adb1ce69d116e306beb8dcf4052524845c98f1c
Pablo Barreyro <Pabarreyro@gmail.com>
2018-04-20 16:53:14 -0700

    add passing code to generate life expectancies on other planets

--- src/space_age.js
+++ src/space_age.js
@@ -1,12 +1,17 @@
 class SpaceAge {
-  constructor(birthYear, birthMonth, birthDay) {
-    this.birthday = new Date(birthYear, birthMonth-1, birthDay);
+  constructor(birthYear, birthMonth, birthDay, lifeExpectancy) {
     this.now = new Date();
+    this.birthday = new Date(birthYear, birthMonth-1, birthDay);
+    this.expectancyEarth = convertYearsToSeconds(lifeExpectancy);
     this.ageEarth = this.calculateAge();
     this.ageMercury = this.ageEarth / 0.24;
+    this.expectancyMercury = Math.floor(this.expectancyEarth / 0.24);
     this.ageVenus = this.ageEarth / 0.62;
+    this.expectancyVenus = Math.floor(this.expectancyEarth / 0.62);
     this.ageMars = this.ageEarth / 1.88;
+    this.expectancyMars = Math.floor(this.expectancyEarth / 1.88);
     this.Jupiter = this.ageEarth / 11.86;
+    this.expectancyJupiter = Math.floor(this.expectancyEarth / 11.86);
   }
 
   calculateAge() {
@@ -16,10 +21,9 @@ class SpaceAge {
     } else {
       age += this.now.getUTCFullYear() - this.birthday.getUTCFullYear();
     }
-    
+
     return convertYearsToSeconds(age);
   }
-
 }
 
 function convertYearsToSeconds(years) {
