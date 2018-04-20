342ce054514cc63a9cee2ba5e1898339fea4522b
Pablo Barreyro <Pabarreyro@gmail.com>
2018-04-20 16:23:54 -0700

    add passing code to generate age in years on other planets

--- src/space_age.js
+++ src/space_age.js
@@ -3,6 +3,10 @@ class SpaceAge {
     this.birthday = new Date(birthYear, birthMonth-1, birthDay);
     this.now = new Date();
     this.ageEarth = this.calculateAge();
+    this.ageMercury = this.ageEarth / 0.24;
+    this.ageVenus = this.ageEarth / 0.62;
+    this.ageMars = this.ageEarth / 1.88;
+    this.Jupiter = this.ageEarth / 11.86;
   }
 
   calculateAge() {
@@ -12,9 +16,10 @@ class SpaceAge {
     } else {
       age += this.now.getUTCFullYear() - this.birthday.getUTCFullYear();
     }
-
-    return convertYearsToSeconds(age);
+    return age;
+    // return convertYearsToSeconds(age);
   }
+
 }
 
 function convertYearsToSeconds(years) {
