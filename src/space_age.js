9e8e88c425ed26cad1a139ec5b2ed3b5589759db
Pablo Barreyro <Pabarreyro@gmail.com>
2018-04-20 17:15:45 -0700

    add passing code to generate remaining years of life on other planets

--- src/space_age.js
+++ src/space_age.js
@@ -7,12 +7,16 @@ class SpaceAge {
     this.lifeRemaining = this.expectancyEarth - this.ageEarth;
     this.ageMercury = this.ageEarth / 0.24;
     this.expectancyMercury = Math.floor(this.expectancyEarth / 0.24);
+    this.lifeRemainingMercury = Math.floor(this.lifeRemaining / 0.24);
     this.ageVenus = this.ageEarth / 0.62;
     this.expectancyVenus = Math.floor(this.expectancyEarth / 0.62);
+    this.lifeRemainingVenus = Math.floor(this.lifeRemaining / 0.62);
     this.ageMars = this.ageEarth / 1.88;
     this.expectancyMars = Math.floor(this.expectancyEarth / 1.88);
+    this.lifeRemainingMars = Math.floor(this.lifeRemaining / 1.88);
     this.Jupiter = this.ageEarth / 11.86;
     this.expectancyJupiter = Math.floor(this.expectancyEarth / 11.86);
+    this.lifeRemainingJupiter = Math.floor(this.lifeRemaining / 11.86);
   }
 
   calculateAge() {
